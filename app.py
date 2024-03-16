2947c5bd456c3aa6acb9ce0af974ed3af111897c
Lukas Pastva <lukas.pastva>
2024-03-16 19:14:38 +0100

    feat: Initial commit.

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def authenticate_with_vault():
 
     if login_response.returncode == 0 and "client_token" in login_response.stdout:
         VAULT_TOKEN = subprocess.run("echo '{}' | jq -r '.auth.client_token'".format(login_response.stdout), shell=True, capture_output=True, text=True).stdout.strip()
-        app.logger.debug("Vault authentication successful.")
+        app.logger.debug(f"Vault authentication successful. Token: {VAULT_TOKEN[:4]}****")  # Logging part of the token for demonstration; avoid logging the full token.
     else:
         app.logger.error(f"Vault authentication failed. Response: {login_response.stdout}, Error: {login_response.stderr}")
 
@@ -36,23 +36,21 @@ def index():
 @app.route('/list-secrets', methods=['GET'])
 def list_secrets():
     parent_path = request.args.get('parent', '')
-    app.logger.debug(f"Attempting to list secrets at path: '{parent_path}'")
+    app.logger.debug(f"Attempting to list secrets at path: '{parent_path}' with token prefix: {VAULT_TOKEN[:4]}****")  # Demonstrative logging
     try:
         list_cmd = f"curl -s --header \"X-Vault-Token: {VAULT_TOKEN}\" {VAULT_ADDR}/v1/kv/metadata/{parent_path}"
+        app.logger.debug(f"Executing command: {list_cmd}")  # Log the command; ensure sensitive data is not logged in production
         list_response = subprocess.run(list_cmd, shell=True, capture_output=True, text=True)
         if list_response.returncode == 0:
             try:
-                # Attempt to parse the JSON response
                 secrets_list = json.loads(list_response.stdout)
                 app.logger.debug(f"Successfully listed secrets at path: '{parent_path}'. Response: {secrets_list}")
                 return jsonify(secrets_list), 200
             except json.JSONDecodeError:
-                # Log and handle JSON parsing error
                 app.logger.error(f"Failed to parse JSON response. Response: {list_response.stdout}")
                 return jsonify({"success": False, "error": "Failed to parse JSON response."}), 500
         else:
-            # Log stderr when the command fails
-            app.logger.error(f"Failed to list secrets. Response: {list_response.stdout}, Error: {list_response.stderr}")
+            app.logger.error(f"Failed to list secrets. Command: {list_cmd}, Response: {list_response.stdout}, Error: {list_response.stderr}")
             return jsonify({"success": False, "error": list_response.stderr}), 500
     except Exception as e:
         app.logger.error(f"Error listing secrets at path: '{parent_path}'. Exception: {e}")
